630363f62d77ae1142a33fe153f1f1de587cd6e5
Joel Sprinkle <joel.sprinkle@carleton.ca>
2017-03-25 13:22:30 -0400

    loads dashboard template

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+var path = require('path');
+
 /*
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World\n");
 });
 */
-console.log(app)
+
 app.get('/', function(req, res){
   //res.writeHead(200, {'Content-Type': 'text/plain'});
   //res.write('Hello World!\n');
   //res.end();
-  res.send('hello World!')
+  //res.send('hello World!')
+  res.sendFile(path.join(__dirname + '/dashboard.htm'));
 });
+
+
 
 //server.listen(8080, 'localhost');
 app.listen(8080, 'localhost');

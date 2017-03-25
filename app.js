5b28f0e72b2705fb9b64608488a84b658714bb4b
Karen Lin <karenxlin@gmail.com>
2017-03-25 14:30:31 -0400

    db connection

--- app.js
+++ app.js
@@ -10,6 +10,30 @@ var server = http.createServer(function (request, response) {
 });
 */
 
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : '54.237.239.59',
+    user     : 'db',
+    password : 'rhok',
+    database    : 'vitaminzang'
+});
+
+connection.connect();
+ 
+var queryString = 'select * from vitaminzang.patient';
+ 
+connection.query(queryString, function(err, rows, fields) {
+    if (err) throw err;
+ 
+    for (var i in rows) {
+        console.log('First name: ', rows[i].firstname);
+    }
+});
+ 
+connection.end();
+
+
 app.get('/', function(req, res){
   //res.writeHead(200, {'Content-Type': 'text/plain'});
   //res.write('Hello World!\n');

d9e7d206aff890a12b8e5c59dc1e1fa89bcb62ab
Joel Sprinkle <joel.sprinkle@carleton.ca>
2017-03-25 17:32:30 -0400

    trying to get data

--- app.js
+++ app.js
@@ -27,40 +27,58 @@ var connection = mysql.createConnection({
 });
 
 connection.connect();
- 
+console.log(1);
 var queryString = 'select * from vitaminzang.patient';
- 
+var data = '';
 connection.query(queryString, function(err, rows, fields) {
     if (err) throw err;
-    var data = '';
+
     for (var i in rows) {
-      data += '<tr><td>R/Y/G</td><td>' + rows[i].firstname + ' ' + rows[i].lastname + '</td><td>' + rows[i].room + '</td><td>' + rows[i].ward + '</td></tr>';
+      this.data += '<tr><td>R/Y/G</td><td>' + rows[i].firstname + ' ' + rows[i].lastname + '</td><td>' + rows[i].room + '</td><td>' + rows[i].ward + '</td></tr>';
     }
+    console.log(2);
+//console.log("hello " + this.data);
 });
- 
+
+console.log(3);
 connection.end();
 
 fs = require('fs');
-fs.readFile('index2.html', 'utf8', function (err,htmlData) {
+var content;
+fs.readFile('index.html', 'utf8', function (err,htmlData) {
+  console.log(4);
     if (err) {
       return console.log(err);
     }
-    console.log(htmlData);
-  });
+    content = htmlData;
+    processFile();
+    //content = htmlData;
+    //supplant({data: data}, htmlData);
+    //this.data = htmlData;
+    //console.log(htmlData);
+});
+function processFile() {
+  //return content;
+    //console.log(content);
+}
+console.log(5);
 
-supplant = function (o) {
-    return this.replace(/{([^{}]*)}/g,
+
+supplant = function (o, htmlData) {
+  console.log(6);
+  htmlData.replace(/{([^{}]*)}/g,
         function (a, b) {
             var r = o[b];
             return typeof r === 'string' || typeof r === 'number' ? r : a;
         }
     );
 };
-
+//console.log(data);
 //console.log("I'm {age} years old!".supplant({ age: 29 }));
 //console.log(htmlData.supplant({data: data}));
 
 app.get('/', function(req, res){
+  console.log(7);
   //res.writeHead(200, {'Content-Type': 'text/plain'});
   //res.write('Hello World!\n');
   //res.end();

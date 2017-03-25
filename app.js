bd3f71629e0280f5fef055373f8a09fced215bde
Karen Lin <karenxlin@gmail.com>
2017-03-25 17:59:43 -0400

    sprintf replace

--- app.js
+++ app.js
@@ -10,13 +10,6 @@ var phoneFrom = '16139092162'; // Sprinkle's zang number
 
 app.use(express.static(__dirname + '/public'))
 
-/*
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
-});
-*/
-
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
@@ -29,63 +22,47 @@ var connection = mysql.createConnection({
 connection.connect();
 console.log(1);
 var queryString = 'select * from vitaminzang.patient';
-var data = '';
+
 connection.query(queryString, function(err, rows, fields) {
     if (err) throw err;
-
+    var data = '';
     for (var i in rows) {
-      this.data += '<tr><td>R/Y/G</td><td>' + rows[i].firstname + ' ' + rows[i].lastname + '</td><td>' + rows[i].room + '</td><td>' + rows[i].ward + '</td></tr>';
+      data += '<tr><td>R/Y/G</td><td>' + rows[i].firstname + ' ' + rows[i].lastname + '</td><td>' + rows[i].room + '</td><td>' + rows[i].ward + '</td></tr>';
     }
     console.log(2);
-//console.log("hello " + this.data);
+
+    fs = require('fs');
+	fs.readFile('index.html', 'utf8', function (err,htmlData) {
+	  console.log(4);
+	    if (err) {
+	      return console.log(err);
+	    }
+	    var sprintf = require("sprintf-js").sprintf;
+
+		htmlData = sprintf(htmlData, data);
+		
+		app.get('/', function(req, res){
+		  console.log(7);
+		  res.writeHead(200, {'Content-Type': 'text/html'});
+		  res.write(htmlData);
+		  res.end();
+
+		  //res.sendFile(path.join(__dirname + '/index.html'));
+		});
+
+		//console.log(htmlData);
+	});
+
+	console.log(5);
 });
 
 console.log(3);
 connection.end();
 
-fs = require('fs');
-var content;
-fs.readFile('index.html', 'utf8', function (err,htmlData) {
-  console.log(4);
-    if (err) {
-      return console.log(err);
-    }
-    content = htmlData;
-    processFile();
-    //content = htmlData;
-    //supplant({data: data}, htmlData);
-    //this.data = htmlData;
-    //console.log(htmlData);
-});
-function processFile() {
-  //return content;
-    //console.log(content);
-}
-console.log(5);
-
-
-supplant = function (o, htmlData) {
-  console.log(6);
-  htmlData.replace(/{([^{}]*)}/g,
-        function (a, b) {
-            var r = o[b];
-            return typeof r === 'string' || typeof r === 'number' ? r : a;
-        }
-    );
-};
 //console.log(data);
 //console.log("I'm {age} years old!".supplant({ age: 29 }));
 //console.log(htmlData.supplant({data: data}));
 
-app.get('/', function(req, res){
-  console.log(7);
-  //res.writeHead(200, {'Content-Type': 'text/plain'});
-  //res.write('Hello World!\n');
-  //res.end();
-  //res.send('hello World!')
-
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
 
 
 var options = {

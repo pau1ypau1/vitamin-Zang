a1e0f020a558b0b4e243e032a70c0f88d2d9f10c
Joel Sprinkle <joel.sprinkle@carleton.ca>
2017-03-25 16:38:44 -0400

    broke it

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+
 app.use(express.static(__dirname + '/public'))
 
 /*
@@ -24,27 +25,56 @@ var connection = mysql.createConnection({
 connection.connect();
  
 var queryString = 'select * from vitaminzang.patient';
+var data;
  
 connection.query(queryString, function(err, rows, fields) {
     if (err) throw err;
- 
+    var data = '';
     for (var i in rows) {
-        console.log('First name: ', rows[i].firstname);
+      data += '<tr><td>R/Y/G</td><td>' + rows[i].firstname + ' ' + rows[i].lastname + 
+        '</td><td>' + rows[i].room + '</td><td>' + rows[i].ward + '</td></tr>';
+        //console.log('First name: ', rows[i].firstname);
     }
+    //console.log(a);
 });
  
 connection.end();
 
+fs = require('fs')
+fs.readFile('index.html', 'utf8', function (err,htmlData) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(htmlData);
+  });
+
+supplant = function (o) {
+    return this.replace(/{([^{}]*)}/g,
+        function (a, b) {
+            var r = o[b];
+            return typeof r === 'string' || typeof r === 'number' ? r : a;
+        }
+    );
+};
+
+//console.log("I'm {age} years old!".supplant({ age: 29 }));
+//console.log(htmlData.supplant({data: data}));
 
 app.get('/', function(req, res){
   //res.writeHead(200, {'Content-Type': 'text/plain'});
   //res.write('Hello World!\n');
   //res.end();
   //res.send('hello World!')
+  //console.log(htmlData.supplant({data: data}));
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-
+/*
+app.post('/data', function(req, res){
+  //res.send(data);
+  console.log(data);
+});
+*/
 
 
 //server.listen(8080, 'localhost');

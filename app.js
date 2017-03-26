6cf542a05fa23b5b1ef223d22c2b82155a34d588
Karen Lin <karenxlin@gmail.com>
2017-03-26 12:27:41 -0400

    code cleanup.

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ var app = express();
 var path = require('path');
 var request = require("request");
 var bodyParser  = require('body-parser');
-
-var phoneTo = '';
-var phoneFrom = '16139092162'; // Sprinkle's zang number
+var sprintf = require("sprintf-js").sprintf;
 
 app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
+// Sprinkle's zang number
+var phoneFrom = '16139092162'; 
 
+// My SQL database connection information
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
     host     : '54.237.239.59',
@@ -23,6 +24,8 @@ var connection = mysql.createConnection({
 });
 
 connection.connect();
+
+// fetching dashboard information
 var queryString = 'select p.firstname, p.lastname, p.room, p.ward, s.status from vitaminzang.patient p left join vitaminzang.medstatus s on p.patientid = s.patientid';
 
 connection.query(queryString, function(err, rows, fields) {
@@ -32,6 +35,7 @@ connection.query(queryString, function(err, rows, fields) {
 
 		data += '<tr><td>';
 
+		// status colouring
     	if (typeof rows[i].status != "undefined") {
     		switch (rows[i].status) {
     			case '1': data += '<img src="/1.png" style="width:100px;" />';
@@ -46,15 +50,17 @@ connection.query(queryString, function(err, rows, fields) {
       data +='</td><td>' + rows[i].firstname + ' ' + rows[i].lastname + '</td><td>' + rows[i].room + '</td><td>' + rows[i].ward + '</td></tr>';
     }
 
+    // fetching page frame
     fs = require('fs');
 	fs.readFile('index.html', 'utf8', function (err,htmlData) {
 	    if (err) {
 	      return console.log(err);
 	    }
-	    var sprintf = require("sprintf-js").sprintf;
-
-		htmlData = sprintf(htmlData, data);
+	    
+	    // insert dynamic data
+		htmlData = this.sprintf(htmlData, data);
 		
+		// Display
 		app.get('/', function(req, res){
 		  res.writeHead(200, {'Content-Type': 'text/html'});
 		  res.write(htmlData);
@@ -66,15 +72,16 @@ connection.query(queryString, function(err, rows, fields) {
 connection.end();
 
 
+// Vitamin-zang api for zang notification replies
 app.get('/vitaminapi', function(req, res){ 
   console.log(req.query);
   res.redirect('/');
 });
 
 
+// Zang api notifications
+var notificationBody = 'Hi, this is a reminder to please take your meds.'; // need to bilingualize app
 app.get('/phNum', function(req, res){ 
-  //console.log(req.query['phoneNum']);
-  
   var options = {
   	method: 'POST',
   	url: 'https://api.zang.io/v2/Accounts/AC7c889084457938dc50ca4f0f8b6bee22/SMS/Messages.json',
@@ -83,7 +90,7 @@ app.get('/phNum', function(req, res){
   	'cache-control': 'no-cache',
   	'content-type': 'application/x-www-form-urlencoded',
   	authorization: 'Basic ' + new Buffer('AC7c889084457938dc50ca4f0f8b6bee22:deb05f2d8251479399ccb3fe2b1078ee', 'utf8').toString('base64') },
-  	form: { To: req.query['phoneNum'], From: phoneFrom, Body: 'Hello, test' }
+  	form: { To: req.query['phoneNum'], From: phoneFrom, Body: notificationBody }
   };
 
   request(options, function (error, response, body) {
@@ -92,14 +99,11 @@ app.get('/phNum', function(req, res){
   	console.log(body);
   });
   
-  
   res.redirect('/');
 });
 
-
-
-
 /*
+var phoneTo = '';
 var options = {
 	method: 'POST',
 	url: 'https://api.zang.io/v2/Accounts/AC7c889084457938dc50ca4f0f8b6bee22/SMS/Messages.json',
